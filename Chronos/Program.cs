58cd49e3775de7c1fe079e0414dbb2047a4dba7a
AndreSebastiao <andresebastiao755@gmail.com>
2024-03-22 23:06:18 +0000

    Chronos exercise done

--- Chronos/Program.cs
+++ Chronos/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Diagnostics;
 
 namespace Chronos
 {
@@ -6,7 +8,27 @@ namespace Chronos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /// <summary>
+            /// Declaring stopwatches
+            /// </summary>
+            Stopwatch crono1 = new Stopwatch();
+            Stopwatch crono2 = new Stopwatch();
+
+            //Controlling the Stopwatches
+            crono1.Start();
+            Thread.Sleep(600);
+            crono2.Start();
+            Thread.Sleep(600);
+            crono1.Stop();
+            crono2.Stop();
+
+            //Reading time of Stopwatches in seconds
+            float t1 = crono1.ElapsedMilliseconds / 1000f;
+            float t2 = crono2.ElapsedMilliseconds / 1000f;
+
+            //Print elapsed time
+            Console.WriteLine($"{t1:f3}");
+            Console.WriteLine($"{t2:f3}");
         }
     }
 }
