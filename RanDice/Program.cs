17157182d5582b67a8eac7aa27308f81b6327873
AndreSebastiao <andresebastiao755@gmail.com>
2024-03-22 23:48:46 +0000

    Random Dice generation

--- RanDice/Program.cs
+++ RanDice/Program.cs
@@ -6,7 +6,29 @@ namespace RanDice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Get bock the number of dies and seed
+            int n = int.Parse(args[0]);
+            int s = int.Parse(args[1]);
+
+            //Random generation based on the seed provided
+            Random random = new Random(s);
+
+            //Declaring variable
+            int diceResult = 0;
+
+            //Loop to add the number from the dies
+            for(int i = 0;i < n; i++)
+            {
+                //Random number between 1-6
+                int randomNumber = random.Next(1, 7);
+
+                //Add result to the final product
+                diceResult += randomNumber;
+
+                //Reset the value of the dice
+                randomNumber = 0;
+            }
+            Console.WriteLine(diceResult);
         }
     }
 }
