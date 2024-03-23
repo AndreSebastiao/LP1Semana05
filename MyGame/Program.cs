713e838c2601431644bbabffc8ba061fa91fb83e
AndreSebastiao <andresebastiao755@gmail.com>
2024-03-23 00:29:31 +0000

    Exercise 10

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -6,7 +6,23 @@ namespace MyGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int numberOfVillains = int.Parse(args[0]);
+
+            Enemy[] villains = new Enemy[numberOfVillains];
+
+            for (int i = 0; i < villains.Length; i++)
+            {
+                Console.Write($"Nome do inimigo {i+1}: ");
+
+                string name = Console.ReadLine();
+
+                villains[i] = new Enemy(name);
+            }
+
+            foreach(Enemy enemy in villains)
+            {
+                Console.WriteLine($"{enemy.GetName()} {enemy.GetHealth()} {enemy.GetShield()}");
+            }
         }
     }
 }
