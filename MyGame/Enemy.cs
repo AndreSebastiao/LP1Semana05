c3fe05ed1f7bb9352a098a179d54652c8828bb45
AndreSebastiao <andresebastiao755@gmail.com>
2024-03-23 00:14:21 +0000

    GetHealth, GetShield and SetName

--- MyGame/Enemy.cs
+++ MyGame/Enemy.cs
@@ -27,5 +27,37 @@ namespace MyGame
                 if (health < 0) health = 0;
             }
         }
+
+        public float GetHealth()
+        {
+            return health;
+        }
+
+        public float GetShield()
+        {
+            return GetShield;
+        }
+
+        public string SetName(string newName)
+        {
+            if (newName.Length > 8)
+            {
+                string temp = "";
+
+                int i = 0;
+
+                foreach(char c in newName)
+                {
+                    if (i < 8)
+                    {
+                        temp += c;
+                        i++;
+                    }
+                }
+                nameof = temp;
+            }
+            else name = newName;
+            return name;
+        }
     }
 }
