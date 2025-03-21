e195e6248d2db248cabcecdda081f29101241d05
AndreSebastiao <andresebastiao755@gmail.com>
2025-03-21 14:14:57 +0000

    Importar bibliotecas e linhas de código obrigatórias

---
+++ WorkerTable/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WorkerTable
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Randomizer.Seed = new Random(((byte)int));
+            Faker faker = new Faker("pt_PT");
+        }
+    }
+}
